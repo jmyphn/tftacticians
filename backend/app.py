776bc93535026835c8b186233726bc868767b508
Jonah Huang <jh2449@cornell.edu>
2025-04-25 02:12:16 -0400

    add flip cards

--- backend/app.py
+++ backend/app.py
@@ -5,13 +5,18 @@ from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import numpy as np
 from dotenv import load_dotenv
-from helpers.utils import (champions_lower_dict, tokenize, recommend_champions, combined_champions)
+from helpers.utils import (
+    champions_lower_dict,
+    tokenize,
+    recommend_champions,
+    combined_champions,
+)
 from helpers.svd import SVDSearcher
 
 # load env variables from .env file
 load_dotenv()
 
-k = 5 # Number of champions to recommend
+k = 5  # Number of champions to recommend
 
 # ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
@@ -21,7 +26,11 @@ os.environ["ROOT_PATH"] = os.path.abspath(os.path.join("..", os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 LOCAL_MYSQL_USER = "root"
-LOCAL_MYSQL_USER_PASSWORD = os.getenv("DB_PASSWORD") if (os.getenv("DB_PASSWORD") != None or os.getenv("DB_PASSWORD") != "") else "admin"
+LOCAL_MYSQL_USER_PASSWORD = (
+    os.getenv("DB_PASSWORD")
+    if (os.getenv("DB_PASSWORD") != None or os.getenv("DB_PASSWORD") != "")
+    else "admin"
+)
 LOCAL_MYSQL_PORT = 3306
 LOCAL_MYSQL_DATABASE = "tftacticians"
 
@@ -52,6 +61,7 @@ trait_info {
 champion_descriptions = combined_champions
 svd_searcher = SVDSearcher(champion_descriptions)
 
+
 # Sample search, the LIKE operator in this case is hard-coded,
 # but if you decide to use SQLAlchemy ORM framework,
 # there's a much better and cleaner way to do this
@@ -62,7 +72,7 @@ def sql_search_champions(champions):
         result = mysql_engine.query_selector(query_sql)
         keys = ["name", "traits"]
         champion_info = champion_info + [dict(zip(keys, row)) for row in result]
-    
+
     # trait info
     keys = ["name", "description"]
     for champion in champion_info:
@@ -75,12 +85,14 @@ def sql_search_champions(champions):
             print(trait)
             query_sql = f"""SELECT * FROM trait_info WHERE LOWER( name ) LIKE '%%{trait.lower()}%%' limit 1"""
             result = mysql_engine.query_selector(query_sql)
-            
+
             trait_info = [dict(zip(keys, row)) for row in result]
             if len(trait_info) > 0:
                 champion["traits"].append(trait_info[0])
             else:
-                champion["traits"].append({"name": trait, "description": "No description found."})
+                champion["traits"].append(
+                    {"name": trait, "description": "No description found."}
+                )
     print(champion_info)
     # Convert the champion_info list to JSON
     # and return it as a response
@@ -96,12 +108,11 @@ def home():
 
 @app.route("/champions")
 def champions_search():
-    query = request.args.get("query", '')
+    query = request.args.get("query", "")
     svd_results_all_champions = svd_searcher.search(query)
     svd_results_all_champions = np.array(svd_results_all_champions).flatten()
 
     tokenized_text = tokenize(query)
-    
 
     # now we find the champions in the text that the user queried.
     # TODO: we can probably use text similarity to find the closest match
@@ -110,14 +121,14 @@ def champions_search():
     for token in tokenized_text:
         if token in champions_lower_dict:
             champions_to_find.append(token.lower())
-    
+
     champions_csv = ",".join(champions_to_find)
 
     # get the recommended units for the user's comp
     recommended_champions = recommend_champions(champions_csv)
 
     # linear combination between the svd results and the recommended champions
-    find_champions = 0.5*svd_results_all_champions + 0.5*recommended_champions
+    find_champions = 0.5 * svd_results_all_champions + 0.5 * recommended_champions
     find_champions = np.argsort(find_champions)[::-1][:k]
     find_champions = [combined_champions[i] for i in find_champions]
     find_champions = [x.split(":")[0].strip() for x in find_champions]
@@ -126,6 +137,26 @@ def champions_search():
 
     # return recommended champions
     return sql_search_champions(find_champions)
+
+
+@app.route("/champion-lore")
+def champion_lore():
+    from flask import jsonify
+    from static.lore import tft_champions  # Import the lore dictionary
+
+    champion_name = request.args.get("name", "")
+
+    # Normalize the champion name to match the format in lore.py
+    normalized_name = champion_name.strip()
+
+    # Find the lore in the tft_champions dictionary
+    lore = "No lore available."
+    for key, value in tft_champions.items():
+        if key.lower() == normalized_name.lower():
+            lore = value
+            break
+
+    return jsonify({"name": champion_name, "lore": lore})
 
 
 if "DB_NAME" not in os.environ:
